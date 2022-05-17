120b2bb2ed8cc43d3bd43463942314cc9fb9f380
Praveensoreng <91107691+Praveensoreng@users.noreply.github.com>
2022-05-17 08:35:34 +0530

    Update ClassCard.js

--- src/components/ClassCard.js
+++ src/components/ClassCard.js
@@ -3,7 +3,7 @@ import './FunctionalCard.css'
 class ClassCard extends React.Component {
     render() {
       return (
-      <div className="card">
+      <div className="classCard">
            <h1>This is Created Using Class Component</h1>
            <p>This is done using external CSS</p>
            <p style={{color:'blue'}}>This is done using inline CSS</p>
